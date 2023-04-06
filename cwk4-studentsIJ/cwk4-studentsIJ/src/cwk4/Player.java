22dbd544d3dc319a315a8fb7fbad7c29bdd8fe4c
Janine <janineobiri@gmail.com>
2023-04-06 13:08:09 +0100

    Janine - Added player class and player role enum class,base classes have constructors

--- cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/Player.java
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/Player.java
@@ -1,11 +1,19 @@
 package cwk4;
 
+import java.util.ArrayList;
+
 public class Player {
     private String name;
     private int warChest;
     private int score;
+    private PlayerRole role;
+
+    private ArrayList<Force> ASF = new ArrayList<Force>();
     public Player(String n, int w, int s)
     {
-
+        name = n;
+        warChest = w;
+        score = s;
+        role = PlayerRole.ADMIRAL;
     }
 }
