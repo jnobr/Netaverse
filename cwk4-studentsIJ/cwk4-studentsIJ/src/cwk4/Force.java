5cb9a71add042906d664864453ca3d19fd310f9c
Janine <janineobiri@gmail.com>
2023-04-06 09:04:41 +0100

    test

---
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/Force.java
@@ -0,0 +1,33 @@
+package cwk4;
+
+public class Force
+{
+    private ForceState state;
+    private String reference;
+    private String name;
+    private int fee;
+    private int strikers;
+    private int laser;
+    private int photon;
+    private int strength;
+    private boolean cloak;
+    private ForceType type;
+
+    public Force(String r, String n, int f, int s, int l, int p, int str, boolean c, ForceType t)
+    {
+        state = new ForceState("In dock");
+        reference = r;
+        name = n;
+        fee = f;
+        strikers = s;
+        laser = l;
+        photon = p;
+        strength = str;
+        cloak = c;
+        type = t;
+
+
+    }
+
+
+}
---
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/ForceType.java
@@ -0,0 +1,23 @@
+package cwk4;
+import java.io.*;
+/**
+ * Enumeration class ForceState
+ *
+ * @author A.Marczyk
+ * @version 02/11/2019
+ */
+public enum ForceType implements Serializable
+{
+    WING("Wing"), WARBIRD("Warbird"), STTARSHIP("Starship");
+    private String type;
+
+    private ForceType(String st)
+    {
+        type = st;
+    }
+
+    public String toString()
+    {
+        return type;
+    }
+}
