d28876d589a9e0dd58055dfac21150419d4b0676
Janine <janineobiri@gmail.com>
2023-04-06 09:21:47 +0100

    Janine

--- cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/Battle.java
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/Battle.java
@@ -2,9 +2,21 @@ package cwk4;
 public class Battle
 
 {
+    private int number;
+    private BattleType type;
+    private String enemy;
+    private int enemyStrength;
+    private int loss;
+    private int gain;
 
-    public Battle()
+    public Battle(int n,BattleType t, String e, int es, int l, int g)
     {
+        number = n;
+        type = t;
+        enemy = e;
+        enemyStrength = es;
+        loss = l;
+        gain = g;
 
     }
 
