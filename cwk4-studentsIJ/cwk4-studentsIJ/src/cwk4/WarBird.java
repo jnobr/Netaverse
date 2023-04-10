6e3a3f03879d02f8d2af0a4500ca88b43da253d8
Lewis Turnbull <lt21aai@herts.ac.uk>
2023-04-10 16:45:38 +0100

    Added getters to Force class and its subclasses.

--- cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/WarBird.java
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/WarBird.java
@@ -4,7 +4,7 @@ package cwk4;
  * @author Andrei Cirlig, Janine Obiri, Lewis Turnbull, Zohaib Rehman
  */
 public class WarBird extends Force {
-    private boolean canCloak;
+    final private boolean canCloak;
 
     public WarBird(String fleetReference, String name, boolean canCloak, int battleStrength) {
         super(fleetReference, name, canCloak ? 400 : 300, battleStrength,
@@ -17,5 +17,9 @@ public class WarBird extends Force {
     public String toString() {
         return super.toString() +
                 "\nCan cloak: " + canCloak;
+    }
+
+    public boolean getCanCloak() {
+        return canCloak;
     }
 }
