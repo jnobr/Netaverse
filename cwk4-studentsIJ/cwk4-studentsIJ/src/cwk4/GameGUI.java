1564fedaf8876872176abe911047ec619cd983de
AndreiCirlig <45592729+AndreiCirlig@users.noreply.github.com>
2023-04-25 23:03:05 +0100

    Update GameGUI.java

--- cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/GameGUI.java
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/GameGUI.java
@@ -1,4 +1,4 @@
-package cwk4; 
+package cwk4;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -6,108 +6,277 @@ import java.util.*;
 
 /**
  * Provide a GUI interface for the game
- * 
+ *
  * @author A.A.Marczyk
  * @version 20/10/23
  */
-public class GameGUI 
-{
+public class GameGUI {
     private WIN gp = new SpaceWars("Horatio");
     private JFrame myFrame = new JFrame("Game GUI");
+    private JPanel centerPanel = new JPanel();
+
+
+    private JButton activateFleetBtn = new JButton("Activate Fleet");
+    private JButton retreatFleetBtn = new JButton("Retreat Fleet");
 
     private JTextArea listing = new JTextArea();
-    private JLabel codeLabel = new JLabel ();
+    private JLabel codeLabel = new JLabel();
     private JButton fightBtn = new JButton("Fight");
     private JPanel eastPanel = new JPanel();
+    private JButton newGameBtn = new JButton("New Game");
+    private JButton loadGameBtn = new JButton("Load Game");
+    private JButton exitBtn = new JButton("Exit");
+    private JLabel creditsLabel = new JLabel("Credits: 0");
 
-    
-    public GameGUI()
-    {
+    public GameGUI() {
         makeFrame();
         makeMenuBar(myFrame);
+        showButtons();
     }
-    
+
     /**
      * Create the main frame's menu bar.
      */
-    private void makeMenuBar(JFrame frame)
-    {
+    private void makeMenuBar(JFrame frame) {
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
-        
+
         // create the Forces menu
         JMenu forcesMenu = new JMenu("Forces");
         menubar.add(forcesMenu);
-        
+
         JMenuItem listForcesItem = new JMenuItem("List All Forces ");
+        JMenuItem close = new JMenuItem("Close");
         listForcesItem.addActionListener(new ListForcesHandler());
         forcesMenu.add(listForcesItem);
 
-
+        // Add the Close menu item and its ActionListener
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listing.setVisible(false);;
+            }
+        });
+        forcesMenu.add(close);
     }
+
     /**
      * Create the Swing frame and its content.
      */
-    private void makeFrame()
-    {    
+    private void makeFrame() {
         myFrame.setLayout(new BorderLayout());
-        myFrame.add(listing,BorderLayout.CENTER);
+        myFrame.add(centerPanel, BorderLayout.CENTER);
+        centerPanel.setLayout(new GridLayout(3, 1));
+        centerPanel.add(newGameBtn);
+        centerPanel.add(loadGameBtn);
+        centerPanel.add(exitBtn);
+        creditsLabel.setVisible(false);
+        myFrame.add(creditsLabel, BorderLayout.WEST);
+        myFrame.add(listing, BorderLayout.NORTH);
         listing.setVisible(false);
         myFrame.add(eastPanel, BorderLayout.EAST);
         // set panel layout and add components
-        eastPanel.setLayout(new GridLayout(4,1));
+        eastPanel.setLayout(new GridLayout(4, 1));
         eastPanel.add(fightBtn);
         fightBtn.addActionListener(new FightBtnHandler());
-        fightBtn.setVisible(true);
-        
-        // building is done - arrange the components and show        
+        eastPanel.setVisible(false);
+
+        // building is done - arrange the components and show
         myFrame.pack();
         myFrame.setVisible(true);
     }
-    
-    
-    private String fighting(int code)
-    {
-        switch (code)
-        {
-            case 0:return "Fight won"; 
-            case 1:return "Fight lost as no suitable force available"; 
-            case 2:return "Fight lost on battle strength, force destroyed";
-            case 3:return "fight is lost and admiral completely defeated ";
+
+    private String fighting(int code) {
+        switch (code) {
+            case 0:
+                return "Fight won";
+            case 1:
+                return "Fight lost as no suitable force available";
+            case 2:
+                return "Fight lost on battle strength, force destroyed";
+            case 3:
+                return "fight is lost and admiral completely defeated ";
         }
         return " no such fight ";
     }
-    
-    private class ListForcesHandler implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e) 
-        { 
+
+    private class ListForcesHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             listing.setVisible(true);
             String xx = gp.getAllForces();
             listing.setText(xx);
-            
+
         }
     }
-    
-    
-    
-    private class FightBtnHandler implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e) 
-        { 
+
+    private class FightBtnHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             int result = -1;
             String inputValue = JOptionPane.showInputDialog("Fight number ?: ");
             int num = Integer.parseInt(inputValue);
             result = gp.doBattle(num);
-            JOptionPane.showMessageDialog(myFrame,fighting(result));    
+            JOptionPane.showMessageDialog(myFrame, fighting(result));
         }
     }
-    
- 
-    
-    public static void main(String[] args)
+    private String getBattleInfo(String[] lines, int startIndex) {
+        StringBuilder battleInfo = new StringBuilder("<html>");
+        for (int i = startIndex; i < startIndex + 5 && i < lines.length; i++) {
+            battleInfo.append(lines[i].trim());
+            if (i < startIndex + 4) {
+                battleInfo.append("<br>");
+            }
+        }
+        battleInfo.append("</html>");
+        return battleInfo.toString();
+    }
+    private String activation(int code)
     {
+        switch (code)
+        {
+            case 0:return "force is activated";
+            case 1:return "force is not in the UFFDock";
+            case 2:return "not enough money";
+            case 3:return "no such force";
+            default: return "Error";
+        }
+    }
+    private void showButtons() {
+        newGameBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String admiralName = JOptionPane.showInputDialog("Enter admiral name: ");
+                gp = new SpaceWars(admiralName);
+
+                JOptionPane.showMessageDialog(myFrame, "New game started!");
+                centerPanel.setVisible(false);
+                loadGameBtn.setVisible(false);
+                exitBtn.setVisible(false);
+                creditsLabel.setText(String.format("Credits: %s", gp.getWarchest()));
+
+                creditsLabel.setVisible(true);  // make the credits label visible
+
+                // Create buttons for each battle number
+                JPanel buttonPanel = new JPanel();
+                buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+
+                JScrollPane scrollPane = new JScrollPane(buttonPanel);
+                scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                scrollPane.setPreferredSize(new Dimension(600, 300));
+
+                String allBattles = gp.getAllBattles();
+                String[] lines = allBattles.split("\n");
+
+
+                for (int i = 0; i < lines.length; i++) {
+                    String line = lines[i].trim();
+                    if (line.startsWith("Battle number")) {
+                        String battleNumber = line.split(":")[1].trim();
+                        int battleNum = Integer.parseInt(battleNumber);
+
+                        String battleInfo = getBattleInfo(lines, i);
+                        JButton btn = new JButton(battleInfo);
+                        btn.setPreferredSize(new Dimension(btn.getPreferredSize().width, 100));
+                        btn.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                int result = -1;
+                                result = gp.doBattle(battleNum);
+                                JOptionPane.showMessageDialog(myFrame, fighting(result));
+                            }
+                        });
+                        buttonPanel.add(btn);
+
+                        // Increment the loop counter to account for the 4 additional lines of battle information.
+                        i += 4;
+                    }
+                }
+                JPanel bottomCenterPanel = new JPanel();
+                bottomCenterPanel.setLayout(new BoxLayout(bottomCenterPanel, BoxLayout.X_AXIS));
+
+                bottomCenterPanel.add(Box.createHorizontalGlue());
+                bottomCenterPanel.add(activateFleetBtn);
+                bottomCenterPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Add some spacing between the buttons
+                bottomCenterPanel.add(retreatFleetBtn);
+                bottomCenterPanel.add(Box.createHorizontalGlue());
+                myFrame.add(bottomCenterPanel, BorderLayout.SOUTH);
+                activateFleetBtn.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        String allForces = gp.getAllForces();
+                        String[] lines = allForces.split("\n");
+
+                        // Create a new JFrame to display the available ships
+                        JFrame activateFleetFrame = new JFrame("Activate Fleet");
+                        activateFleetFrame.setLayout(new BorderLayout());
+
+                        JPanel shipsPanel = new JPanel();
+                        shipsPanel.setLayout(new BoxLayout(shipsPanel, BoxLayout.Y_AXIS));
+
+                        JScrollPane scrollPane = new JScrollPane(shipsPanel);
+                        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                        scrollPane.setPreferredSize(new Dimension(600, 300));
+
+                        for (int i = 0; i < lines.length; i++) {
+                            String line = lines[i].trim();
+                            if (line.startsWith("Fleet reference")) {
+                                StringBuilder shipInfo = new StringBuilder("<html>");
+                                for (int j = i; j < i + 9 && j < lines.length; j++) {
+                                    shipInfo.append(lines[j].trim());
+                                    if (j < i + 8) {
+                                        shipInfo.append("<br>");
+                                    }
+                                }
+                                shipInfo.append("</html>");
+                                JButton shipButton = new JButton(shipInfo.toString());
+                                shipButton.setPreferredSize(new Dimension(shipButton.getPreferredSize().width, 150));
+                                shipButton.addActionListener(new ActionListener() {
+                                    public void actionPerformed(ActionEvent e) {
+                                        // Implement the activation logic for the ship here
+                                        int res = gp.activateForce(line.split(":")[1].trim());
+                                        System.out.println(activation(res));
+                                        creditsLabel.setText(String.format("Credits: %s", gp.getWarchest()));
+                                    }
+                                });
+
+                                shipsPanel.add(shipButton);
+                                i += 8; // Increment the loop counter to account for the additional lines of ship information.
+                            }
+                        }
+
+                        activateFleetFrame.add(scrollPane, BorderLayout.CENTER);
+                        activateFleetFrame.pack();
+                        activateFleetFrame.setVisible(true);
+                    }
+                });
+
+                centerPanel.setLayout(new GridBagLayout());
+                GridBagConstraints gbc = new GridBagConstraints();
+                gbc.gridx = 0;
+                gbc.gridy = 0;
+                gbc.weightx = 1;
+                gbc.weighty = 1;
+                gbc.fill = GridBagConstraints.NONE;
+                centerPanel.add(scrollPane, gbc);
+                newGameBtn.setVisible(false);
+                centerPanel.revalidate();
+                centerPanel.repaint();
+                centerPanel.setVisible(true);
+            }
+        });
+
+        loadGameBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // code to load a saved game
+            }
+        });
+
+        exitBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
         new GameGUI();
     }
 }
-   
